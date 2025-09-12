3828c9777a33b7a4f32cd8aea47ea4eb4d3f737f
milan <milanraj@techgropse.com>
2025-09-12 14:06:11 +0530

    Suggestions

---
+++ conditionals/suggestionSystem.py
@@ -0,0 +1,8 @@
+snack = input("Enter Your Preffred Snack: ").lower()
+
+print(f"user Said: {snack}")
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great Choice !! Well serve you {snack}")
+else :
+    print("Sorry We Only Serve Cookies and Sammosa With Tea")
