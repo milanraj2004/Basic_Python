c42934dcf142bf30535a240661706aabcefaa194
milan <milanraj@techgropse.com>
2025-09-12 14:14:23 +0530

    priceCalculator

---
+++ conditionals/priceCalculator.py
@@ -0,0 +1,10 @@
+cup = input("Choose your Cup Size --- (Small/ Medium/Large)").lower()
+
+if cup == "small":
+    print("Price is 10 Rupees Sir/Ma'am!!!")
+elif cup == "medium":
+    print("Price is 20 Rupees Sir/Ma'am!!!")
+elif cup == "large":
+    print("Price is 30 Rupees Sir/Ma'am!!!")
+else:
+    print("Unknown Cup Size Sir/Ma'am!!!")
