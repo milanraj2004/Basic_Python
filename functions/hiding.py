1bb7c0b7914ebeeb9a5c8156c85b4c92c611ed8b
Milan Raj <milanrajumadp@gmail.com>
2025-09-22 00:47:31 +0530

    Hiding Data

---
+++ functions/hiding.py
@@ -0,0 +1,17 @@
+
+def getInput():
+    print("Getting User Input")
+
+def validateInput():
+    print("Validating User Input")
+
+def saveTODatabase():
+    print("Saving to Database")
+
+def registerUser():
+    getInput()
+    validateInput()
+    saveTODatabase()
+    print("User Registered Successfully")
+
+registerUser()
