afbc334cb5ad83eab507d84d549240c9aa619de0
milan <milanrajumadp@gmail.com>
2025-12-10 23:25:43 +0530

    function return type

--- functions/inputParams.py
+++ functions/inputParams.py
@@ -22,3 +22,28 @@ def make_chai(tea,milk,sugar):
 make_chai("Darjelling","yes","Low") #positional arguments
 
 make_chai(tea="Darjelling",sugar="Low",milk="yes") #keyword arguments)
+
+def special_chai(*ingredients,**extras):
+    print("Ingredients:", ingredients)
+    print("Extras:", extras)
+
+special_chai("cinnamon","cardamom",sweetner="Honey",size= "Large")
+
+def chai_order(order = []):
+     order.append("Ginger chai")
+     print("Order:", order)
+
+chai_order()
+chai_order()
+
+
+def chai_order_fixed(orders = None):
+     if orders is None:
+         orders = []
+   
+     print("Orderssssssssssssssssssssss:", orders)
+     print(orders)
+     orders.append("Ginger chai")
+     print("New Orderssssssssssssssssssssss:", orders)
+chai_order_fixed()
+chai_order_fixed()
