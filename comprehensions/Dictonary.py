c16646ac3cf04366cff04d99bf65641fc5f51117
milan <milanrajumadp@gmail.com>
2026-03-20 15:10:34 +0530

    fixes

---
+++ comprehensions/Dictonary.py
@@ -0,0 +1,9 @@
+tea_prices_inr = {
+    "Masala Chai": 50,
+    "Green Tea": 40,
+    "Lemon Tea": 45,
+}
+""
+"All Prices need to be converted to USD with a conversion rate of 1 INR = 0.012 USD"
+""
+tea_prices
