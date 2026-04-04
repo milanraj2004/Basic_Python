eaf2ce758685ef7c6be116271c5687acf363eddc
milan <milanrajumadp@gmail.com>
2026-04-04 18:52:49 +0530

    fixes

---
+++ cooking_business/main.py
@@ -0,0 +1,9 @@
+# # import recipes.flavors
+# # print(recipes.flavors.veg_Foods())
+# # print(recipes.flavors.non_veg_Foods())
+
+# # from recipes.flavors import veg_Foods, non_veg_Foods
+# # print(veg_Foods())
+# # print(non_veg_Foods())
+
+# from .recipes.flavors import veg_Foods, non_veg_Foods
