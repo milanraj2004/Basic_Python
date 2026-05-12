988654abff54e5608e73037703782002aa2dcfb2
Milan Raj <milanrajumadp@gmail.com>
2026-05-12 13:41:19 +0530

    fixes

--- oops/BaseClass.py
+++ oops/BaseClass.py
@@ -11,7 +11,7 @@ class Milan:
 #         self.type_ = type_
 #         self.strength = strength
 #         self.level = level
-
+#
 
 # class Delhi(Milan):
 #     def __init__(self, type_, strength, level):
